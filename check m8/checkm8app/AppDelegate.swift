8a7a7432a636d6da07c5c0be690d30a3f81219d0
mikiaszeleke <70564204+mikiaszeleke@users.noreply.github.com>
2020-12-16 23:13:15 -0800

    newly updated entire app!
    The somewhat fixed version of check m8

---
+++ check m8/checkm8app/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let window = UIWindow()
+    let locationService = LocationService()
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
